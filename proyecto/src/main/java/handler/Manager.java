47cf126d74321832ed21e7652acea70a033db347
gerardlloret <gerardlloreteij@gmail.com>
2020-05-16 19:03:42 +0200

    Manager

---
+++ proyecto/src/main/java/handler/Manager.java
@@ -0,0 +1,48 @@
+
+package handler;
+
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import persistencia.DAO;
+
+
+public class Manager {
+    public static DAO bbdd;
+    private static String usuario;
+    
+    //Set usuario logueado
+    public static void setUsuario(String loginUser){
+        usuario = loginUser;
+    }
+    
+    //Get usuario logueado
+    public static String getUsuario(){
+        return usuario;
+    }
+    
+    //Metodo para crear la instancia de la clase DAO y conectar
+    public static void connectar() throws SQLException{
+            bbdd = new DAO();
+            bbdd.conectar();   
+    }
+    
+    //Metodo para desconectar
+    public static void desconectar() throws SQLException{
+            bbdd.desconectar();
+    }
+       
+    //Recibe un date y devuelve un String con el formato HH:mm:ss
+    public static String DateToTime(Date date){
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        String time = sdf.format(date);
+        return time;
+    } 
+    
+    //Recibe un date y devuelve un String con el formato YYYY-MM-dd
+    public static String DateToSqlDate(Date date){
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+        String sqlDate = sdf.format(date);
+        return sqlDate;
+    }
+}
--- proyecto/src/main/java/persistencia/DAO.java
+++ proyecto/src/main/java/persistencia/DAO.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package persistencia;
 
 import java.sql.Connection;
@@ -11,10 +7,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author gerar
- */
+
 public class DAO {
     Connection conexion;
     //Metodo para conectar con la base de datos
